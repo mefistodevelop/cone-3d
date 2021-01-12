a949fab98b1e60fc7a006b3a648a106d8722fd9e
Oleg Rudenko <oleg.ru93@yandex.ru>
2021-01-12 17:00:12 +0300

    feat: add error 404 page

--- src/client/App.js
+++ src/client/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import { Header } from './components/Header/Header';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
 import { Home } from './pages/Home/Home';
 
 export const App = () => (
@@ -11,7 +12,7 @@ export const App = () => (
       <Switch>
         <Route exact path="/" render={() => <Home />} />
         <Route path="/about" render={() => <h1>About</h1>} />
-        <Route path="/*" render={() => <h1>404</h1>} />
+        <Route path="/*" render={() => <ErrorPage />} />
       </Switch>
     </div>
   </div>
